54b9f0dfd23a26b28ccccd5efb98ca38000af71f
anandhu-eng <anandhukicks@gmail.com>
2022-01-29 10:27:28 +0530

    changes made

--- app.py
+++ app.py
@@ -5,3 +5,16 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template('index.html')
+
+@app.route("/reciever")
+def rec():
+    return render_template('receiver.html')
+
+@app.route("/server")
+def ser():
+    return render_template('server.html')
+
+
+
+#to go to the virtualenv: source venv/Scripts/activate
+#to deactivate: deactivate
